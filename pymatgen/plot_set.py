b593ce504129d0403b3d8069ca7ebceee580c3e4
vishankkumar <vishank.kumar@uclouvain.be>
2019-08-30 16:42:41 +0200

    Adding plot_set.py for matplotlib settings

--- pymatgen/plot_set.py
+++ pymatgen/plot_set.py
@@ -7,7 +7,7 @@ def set_mpl(scale=2):
     scale = scale
     mpl.rcParams['figure.figsize'] = [scale * 3.5, scale * 3.5 / 1.4]
     mpl.rcParams['font.size'] = scale * 11
-    mpl.rc('font', family='serif', serif='CMU Serif')
+    mpl.rc('font', family='serif', serif='DejaVu Sans')
     mpl.rc('xtick', labelsize=scale * 10)
     mpl.rc('ytick', labelsize=scale * 10)
     mpl.rc('axes', labelsize=scale * 11)
